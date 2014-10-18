a442355e7f83e862a51c558e195b906b65acd115
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2014-10-18 22:53:45 +0200

    Updated docs and added brand new tryout.

---
+++ tryout/js/index.js
@@ -0,0 +1,21 @@
+function smartTrans (selector, className) {
+	$(selector).addClass(className);
+	$(selector).bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function () {
+		$(selector).css({ overflow: "auto" });
+	});
+}
+
+$(function () {
+	var editor = ace.edit("editor");
+	editor.getSession().setMode("ace/mode/javascript");
+	editor.moveCursorTo(0, 30);
+	editor.focus();
+	
+	setTimeout(function () {
+		smartTrans("h1, input", "animate");
+	}, 0);
+	
+	$("#runButton").click(function () {
+		eval(editor.getValue());
+	});
+});
