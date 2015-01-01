a33966b95dd8865dc26645db444ade6c4562ad66
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2015-01-01 17:14:54 +0100

    Start animating of frameworks in header immediately.
    This bugged me too long.

--- js/index.js
+++ js/index.js
@@ -6,11 +6,13 @@ var items = [
 var current = 0;
 
 $(function () {
-	setInterval(function () {
+	var animateFrameworks;
+	setInterval(animateFrameworks = function () {
 		$("div#frameworks").animate({ opacity: 0 }, 1000, function (){
 			$(this)
 				.text(items[current++ % items.length])
 				.animate({opacity: 1}, 1000);
 		});
 	}, 3000);
+	animateFrameworks();
 });
