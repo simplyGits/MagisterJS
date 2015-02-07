248ad4be73c602293b06448508310444793668f4
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2015-02-07 18:30:32 +0100

    Update old site default / example code and clean up HTML/JS.

--- tryout/js/index.js
+++ tryout/js/index.js
@@ -1,20 +1,13 @@
-function smartTrans (selector, className) {
-	$(selector).addClass(className);
-	$(selector).bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function () {
-		$(selector).css({ overflow: "auto" });
-	});
-}
-
 $(function () {
 	var editor = ace.edit("editor");
 	editor.getSession().setMode("ace/mode/javascript");
 	editor.moveCursorTo(1, 14);
 	editor.focus();
-	
+
 	setTimeout(function () {
-		smartTrans("h1, input", "animate");
+		$("h1, input").addClass("animate");
 	}, 0);
-	
+
 	$("#runButton").click(function () {
 		eval(editor.getValue());
 	});
