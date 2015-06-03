ebbe1dc1f06f16aad90642e0758b715b4d58f874
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2015-06-03 15:54:01 +0200

    up.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: "simply:magisterjs",
-	version: "1.3.3",
+	version: "1.4.0",
 	summary: "A JavaScript implementation of the Magister 6 API",
 	git: "https://github.com/simplyGits/MagisterJS",
 	documentation: "README.md"
