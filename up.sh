92ff63b7e5828178b16df50f9752bc406e0c858e
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2015-08-31 22:09:19 +0200

    add up.sh

---
+++ up.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+OLDVERSION=$(jq -r '.version' ./package.json)
+read -p "New version (old was $OLDVERSION): " NEWVERSION
+
+git stash save
+sed -i "s/$OLDVERSION/$NEWVERSION/g" package.js{on,} src/{yuidoc.json,Magister.coffee}
+grunt
+mocha && git commit -am $"grunt & up to $NEWVERSION"
+git stash show -p | git apply && git stash drop
+git reset .
