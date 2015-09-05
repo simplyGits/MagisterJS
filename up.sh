49fa8dcb2914c38d56d4dfdf54a5a79467489b50
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2015-09-05 11:35:12 +0200

    add meteor and npm publish to up.sh

--- up.sh
+++ up.sh
@@ -6,6 +6,6 @@ read -p "New version (old was $OLDVERSION): " NEWVERSION
 git stash save
 sed -i "s/$OLDVERSION/$NEWVERSION/g" package.js{on,} src/{yuidoc.json,Magister.coffee}
 grunt
-mocha && git commit -am $"grunt & up to $NEWVERSION"
+mocha && git commit -am "grunt & up to $NEWVERSION" && meteor publish && npm pub
 git stash show -p | git apply && git stash drop
 git reset .
