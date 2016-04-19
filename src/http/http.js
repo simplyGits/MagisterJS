8ff5de80882a55d5a83f250820a805f0766a63f9
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-04-19 17:13:53 +0200

    cleanup http.js

--- src/http/http.js
+++ src/http/http.js
@@ -1,51 +1,49 @@
 /*
-	HTTP CLASS
-	======================
-
-	You have to implement this with your own serverside implementation.
-	Beneath here are the requirements you have to follow.
-
- 	======================
-   MINIMAL REQUIREMENTS
- 	======================
- 	callback: function (error, result) {...}
- 	result: { content (string), headers (dictionary), statusCode (number) }
- 	options: { headers (dictionary), data (object) }
-
- 	get(url, options*, callback)
- 	delete(url, options*, callback)
- 	post(url, data, options*, callback)
- 	put(url, data, options*, callback)
-
- 	* = optional (Fill with default value if null (object) ex.: options ?= {})
-
- 	Class holds variable _cookie which is required to be added to the headers
+ * ======================
+ *       HTTP CLASS
+ * ======================
+ *
+ * You have to implement this with your own serverside implementation.
+ * Beneath here are the requirements you have to follow.
+ *
+ * ======================
+ *  minimal requirements
+ * ======================
+ * callback: function (error, result) {...}
+ * result: { content (string), headers (dictionary), statusCode (number) }
+ * options: { headers (dictionary), data (object) }
+ *
+ * get(url, options*, callback)
+ * delete(url, options*, callback)
+ * post(url, data, options*, callback)
+ * put(url, data, options*, callback)
+ *
+ * * = optional (Fill with default value if null (object) ex.: options ?= {})
+ *
+ * Class holds variable _cookie which is required to be added to the headers
 */
 (function() {
+	'use strict';
 	if (!this.Magister) this.Magister = {};
+	function MagisterHttp() {}
 
-	this.Magister.MagisterHttp = (function() {
-			function MagisterHttp() {}
+	MagisterHttp.prototype.get = function(url, options, callback) {
+		// FILL THIS IN
+	};
 
-			MagisterHttp.prototype.get = function(url, options, callback) {
+	MagisterHttp.prototype['delete'] = function(url, options, callback) {
+		// FILL THIS IN
+	};
 
-			};
+	MagisterHttp.prototype.post = function(url, data, options, callback) {
+		// FILL THIS IN
+	};
 
-			MagisterHttp.prototype["delete"] = function(url, options, callback) {
+	MagisterHttp.prototype.put = function(url, data, options, callback) {
+		// FILL THIS IN
+	};
 
-			};
+	MagisterHttp.prototype._cookie = '';
 
-			MagisterHttp.prototype.post = function(url, data, options, callback) {
-
-			};
-
-			MagisterHttp.prototype.put = function(url, data, options, callback) {
-
-			};
-
-			MagisterHttp.prototype._cookie = "";
-
-			return MagisterHttp;
-
-	})();
+	this.Magister.MagisterHttp = MagisterHttp;
 }).call(this);
