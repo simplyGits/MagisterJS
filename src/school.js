a5f6617160c8ffd85472861814280b689e5e43f3
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-07-24 00:08:57 +0200

    add `School#versionInfo`

--- src/school.js
+++ src/school.js
@@ -1,5 +1,8 @@
 'use strict'
 
+import fetch from 'node-fetch'
+import * as util from './util'
+
 /**
  * @class School
  * @constructor
@@ -13,5 +16,18 @@ export default class School {
 		this.id = raw.Id
 		this.name = raw.Name
 		this.url = raw.Url
+	}
+
+	versionInfo() {
+		const url = `${this.url}/api/versie`
+		return fetch(url)
+		.then(res => res.json())
+		.then(obj => ({
+			core: obj.CoreVersie,
+			api: obj.ApiVersie,
+			db: obj.DatabaseVersie,
+			product: obj.ProductVersie,
+			releasedOn: util.parseDate(obj.ReleaseDatum),
+		}))
 	}
 }
