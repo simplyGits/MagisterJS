bb49461cfc3701331470c71d04c04165b7a005b9
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-07-23 23:55:34 +0200

    update up.sh

--- up.sh
+++ up.sh
@@ -3,8 +3,10 @@
 OLDVERSION=$(jq -r '.version' ./package.json)
 read -p "New version (old was $OLDVERSION): " NEWVERSION
 
+rm -rf lib/
 git stash save
-sed -i "s/$OLDVERSION/$NEWVERSION/g" package.json src/{yuidoc.json,Magister.js}
-npm test && git commit -am "up to $NEWVERSION" && git tag "$NEWVERSION" && git push && git push --tags && npm pub
-git stash show -p | git apply && git stash drop
+sed -i "s/$OLDVERSION/$NEWVERSION/g" package.json src/yuidoc.json
+npm test && git commit -Sam "up to $NEWVERSION" && git tag -s "$NEWVERSION" && git push && git push --tags && npm pub
+rm -rf lib/
+git stash pop
 git reset .
