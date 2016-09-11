441d8bbe27082ad9cb79cef5eca0f72f33de4250
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-09-11 15:49:24 +0200

    add some functions to Message

--- src/message.js
+++ src/message.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import MagisterThing from './magisterThing'
 
 /**
@@ -10,5 +11,41 @@ import MagisterThing from './magisterThing'
 export default class Message extends MagisterThing {
 	constructor(magister) {
 		super(magister)
+	}
+
+	/**
+	 * @method addRecipient
+	 * @param {Person|Person[]} recipients
+	 */
+	addRecipient(recipients) {
+		if (!Array.isArray(recipients)) {
+			recipients = [ recipients ]
+		}
+
+		this.recipients = this.recipients.concat(recipients)
+	}
+
+	// REVIEW
+	move(destination) {
+		if (_.isObject(destination)) {
+			destination = destination.id
+		}
+
+		if (this.folderId === destination) {
+			return Promise.resolve(undefined)
+		}
+
+		this.folderId = destination
+		this.saveChanges()
+	}
+
+	/**
+	 * @method remove
+	 * @return {Promise<Error|undefined>}
+	 */
+	remove() {
+		const url = `${this._magister._personUrl}/berichten/${this.id}`
+		return this._magister._privileges.needs('berichten', 'delete')
+		.then(() => this._magister.http.delete(url))
 	}
 }
