dc8517b58bec0dafd41b6e06d43d17de212d4dd3
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-10-18 17:23:18 +0200

    Revert "up.sh: sign grunt commit and tag"
    This reverts commit 4255adb876ae4b5c0b4174c933235c98e70dbc47.

--- up.sh
+++ up.sh
@@ -6,6 +6,6 @@ read -p "New version (old was $OLDVERSION): " NEWVERSION
 git stash save
 sed -i "s/$OLDVERSION/$NEWVERSION/g" package.js{on,} src/{yuidoc.json,Magister.coffee}
 grunt
-mocha && git commit -Sam "grunt & up to $NEWVERSION" && git tag -s "$NEWVERSION" && git push && git push --tags && meteor publish && npm pub
+mocha && git commit -am "grunt & up to $NEWVERSION" && git tag "$NEWVERSION" && git push && git push --tags && meteor publish && npm pub
 git stash show -p | git apply && git stash drop
 git reset .
