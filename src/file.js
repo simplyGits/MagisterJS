5f4e3ba3d9dc837b656e07bafa459b748946f31c
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-11-04 11:30:26 +0100

    ready up File

--- src/file.js
+++ src/file.js
@@ -16,58 +16,128 @@ class File extends MagisterThing {
 	constructor(magister, fileFolder, raw) {
 		super(magister)
 
+		/**
+		 * @type String
+		 * @readonly
+		 */
 		this.id = raw.Id.toString()
-		this.type = raw.BronSoort
+		/**
+		 * @type Number
+		 * @readonly
+		 */
+		this.type = raw.BronSoort // REVIEW: string?
+		/**
+		 * @type String
+		 * @readonly
+		 */
 		this.name = raw.Naam
+		/**
+		 * @type String
+		 * @readonly
+		 */
 		this.uri = raw.Uri
+		/**
+		 * @type Number
+		 * @readonly
+		 */
 		this.size = raw.Grootte
+		// REVIEW
+		/**
+		 * @type Number
+		 * @readonly
+		 */
 		this.rights = raw.Privilege
+		/**
+		 * @type String
+		 * @readonly
+		 */
 		this.mime = raw.ContentType
 
+		/**
+		 * @type Date
+		 * @readonly
+		 */
 		this.changedDate = util.parseDate(raw.GewijzigdOp)
+		/**
+		 * @type Date
+		 * @readonly
+		 */
 		this.creationDate = util.parseDate(raw.GemaaktOp || raw.Datum)
 
-		const addedBy = new Person(magister, null, '', '')
-		addedBy.fullName = raw.GeplaatstDoor
-		this.addedBy = addedBy
+		/**
+		 * @type Person
+		 * @readonly
+		 */
+		this.addedBy = (function () {
+			const p = new Person(magister, null, '', '')
+			p.fullName = raw.GeplaatstDoor
+			return p
+		})()
 
+		/**
+		 * @type String
+		 * @readonly
+		 */
 		this.fileBlobId = util.toString(raw.FileBlobId)
+		/**
+		 * @type FileFolder
+		 * @readonly
+		 */
 		this.fileFolder = fileFolder
+		/**
+		 * @type String
+		 * @readonly
+		 */
 		this.uniqueId = raw.UniqueId
+		/**
+		 * @type String
+		 * @readonly
+		 */
 		this.referenceId = util.toString(raw.Referentie)
 
-		let l = _.find(raw.Links, { Rel: 'Contents' })
-		l = l || _.find(raw.Links, { Rel: 'Self' })
-		if (/^https?/.test(l.Href)) {
-			this._downloadUrl = l.Href
-		} else {
-			this._downloadUrl = magister.school.url + l.Href
+		/**
+		 * @type String
+		 * @readonly
+		 * @private
+		 */
+		this._downloadUrl = undefined
+
+		const fullUrl = url => {
+			url = url.Href
+			return /^https?/.test(url) ? url : magister.school.url + url
 		}
+
+		const selfUrl = _.find(raw.Links, { Rel: 'Self' })
+		const contentUrl = _.find(raw.Links, { Rel: 'Contents' })
+
+		/**
+		 * @type String
+		 * @readonly
+		 */
+		this._selfUrl = fullUrl(selfUrl)
+		/**
+		 * @type String
+		 * @readonly
+		 */
+		this._downloadUrl = fullUrl(contentUrl || selfUrl)
 	}
 
 	/**
-	 * Downloads the current file
-	 * @method download
-	 * @return {Promise<Error|Blob>}
+	 * Opens a stream to the current file
+	 * @return {Promise<Stream>}
 	 */
 	download() {
 		return this._magister.get(this._downloadUrl)
-		.then(res => res.blob())
-	}
-
-	// REVIEW: is this method useful?
-	move(fileFolder) {
-		this.fileFolder = fileFolder
-		return this.saveChanges()
+		.then(res => res.body)
 	}
 
 	/**
 	 * Removes the current file permanently
-	 * @method remove
-	 * @return {Promise<Error|undefined>}
+	 * @return {Promise<undefined>}
 	 */
 	remove() {
-		this._magister.http.delete(`${this._magister._personUrl}/bronnen/${this.id}`)
+		return this._magister._privileges.needs('bronnen', 'delete')
+		.then(() => this._magister.http.delete(this._selfUrl))
 		.then(() => undefined) // throw away the useless result from magister. (current object)
 	}
 
@@ -77,7 +147,31 @@ class File extends MagisterThing {
 	 * @return {Promise<undefined>}
 	 */
 	saveChanges() {
+		return this._magister._privileges.needs('bronnen', 'update')
+		.then(() => this._magister.http.put(this._selfUrl, this._toMagister()))
+		.then(() => undefined)
+	}
 
+	/**
+	 * @private
+	 * @return {Object}
+	 */
+	_toMagister() {
+		const toNumberSafe = val => val == null ? val : parseInt(val, 10)
+
+		return {
+			Id: parseInt(this.id, 10),
+			BronSoort: this.type,
+			Naam: this.name,
+			Uri: this.uri,
+			Grootte: this.size,
+			Privilege: this.rights,
+			ContentType: this.mime,
+			FileBlobId: toNumberSafe(this.fileBlobId),
+			ParentId: this.fileFolder.id,
+			UniqueId: this.uniqueId,
+			Referentie: toNumberSafe(this.referenceId),
+		}
 	}
 }
 
