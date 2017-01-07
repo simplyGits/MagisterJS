f650766fd75d8873d74162eba750fd76cd1ed614
Sander Laarhoven <sander@laarhoven.org>
2017-01-07 18:00:15 +0100

    Convert spaces to tabs (2)

--- _includes/example1.js
+++ _includes/example1.js
@@ -1,10 +1,10 @@
 new Magister.Magister({
-  school: 'xxxx',
-  username: 'xxxx',
-  password: 'xxxx'
+	school: 'xxxx',
+	username: 'xxxx',
+	password: 'xxxx'
 }).ready(function (error) {
-  if (error) return console.log('oops!', error);
-  this.appointments(new Date(), function (error, result) {
-    console.log(result[0].teachers()[0].fullName());
-  });
+	if (error) return console.log('oops!', error);
+	this.appointments(new Date(), function (error, result) {
+		console.log(result[0].teachers()[0].fullName());
+	});
 });
