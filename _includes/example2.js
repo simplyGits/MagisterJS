7059373c578740367f5bc9c527d59fb21547d548
Sander Laarhoven <sander@laarhoven.org>
2017-01-07 17:09:35 +0100

    Move JS examples to includes folder.

---
+++ _includes/example2.js
@@ -0,0 +1,10 @@
+new Magister.Magister({
+  school: 'xxxx',
+  username: 'xxxx',
+  password: 'xxxx'
+}).ready(function (error) {
+  if (error) {
+    return console.log('oops!', error);
+  }
+  this.composeAndSendMessage("subject", "body", ["Thomas", "Tom", "Jesse"]);
+});
