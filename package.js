274a980e802e5f50c8115a294175ca43756cfc6b
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2017-03-26 17:59:43 +0200

    grunt & up to 1.24.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: "simply:magisterjs",
-	version: "1.23.3",
+	version: "1.24.0",
 	summary: "A JavaScript implementation of the Magister 6 API",
 	git: "https://github.com/simplyGits/MagisterJS",
 	documentation: "README.md"
--- src/Magister.coffee
+++ src/Magister.coffee
@@ -14,7 +14,7 @@ else
 # @final
 # @type String
 ###
-root.VERSION = "1.23.3"
+root.VERSION = "1.24.0"
 
 ###*
 # A JavaScript implementation of the Magister 6 API.
