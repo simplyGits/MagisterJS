3e7df08fa15fa7bf02598a3600521443d8c14044
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2018-11-05 13:22:03 +0100

    update docs for 2.0.0-alpha.1

--- _includes/example2.js
+++
@@ -1,8 +0,0 @@
-new Magister.Magister({
-	school: 'xxxx',
-	username: 'xxxx',
-	password: 'xxxx'
-}).ready(function (error) {
-	if (error) return console.log('oops!', error);
-	this.composeAndSendMessage("subject", "body", ["Thomas", "Tom", "Jesse"]);
-});
