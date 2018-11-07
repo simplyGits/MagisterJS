4186b1e223b0a207263bd5a464e91f461595b7e7
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2018-11-07 15:54:51 +0100

    update up.sh

--- up.sh
+++ up.sh
@@ -4,9 +4,14 @@ OLDVERSION=$(jq -r '.version' ./package.json)
 read -p "New version (old was $OLDVERSION): " NEWVERSION
 
 rm -rf lib/
-git stash save
-sed -i "s/$OLDVERSION/$NEWVERSION/g" package.json src/yuidoc.json
-npm test && git commit -Sam "up to $NEWVERSION" && git tag -s "$NEWVERSION" && git push && git push --tags && npm pub
+git stash save 'up.sh auto-stash'
+sed -i "s/$OLDVERSION/$NEWVERSION/g" package.json
+npm test && \
+	git commit -S -a -m "up to $NEWVERSION" && \
+	git tag -s "$NEWVERSION" -m "$NEWVERSION" && \
+	git push && \
+	git push --tags && \
+	npm pub
 rm -rf lib/
 git stash pop
 git reset .
