cbaccb6e9c7c7b9d7427fae9a98230e9cda084a3
Robbert Klarenbeek <robbertkl@renbeek.nl>
2019-07-04 15:27:19 +0200

    Fix response error handling
    Co-Authored-By: Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>

--- src/http.js
+++ src/http.js
@@ -112,16 +112,17 @@ class Http {
 		}
 
 		try {
-			const body = await res.body()
-			const parsed = JSON.parse(body)
+			const parsed = await res.json()
 			if ('SecondsLeft' in parsed) {
 				// Handle rate limit errors
 				this._setRatelimitTime(Number.parseInt(parsed.SecondsLeft, 10))
 				return this._request(obj)
+			} else if ('error' in parsed) {
+				error.message = parsed.error
 			}
-		} catch (_) {
-			return res
-		}
+		} catch (_) {}
+
+		return res
 	}
 
 	/**
