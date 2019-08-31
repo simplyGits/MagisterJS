4b5d6602ba6acc0bd3045495f742b1cb0382de03
idiidk <dviide@protonmail.com>
2019-08-31 19:27:55 +0200

    Removed unnessecary util functions

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,11 +1,3 @@
-import { URL } from 'url'
-
 export function cloneClassInstance(object) {
 	return Object.assign(Object.create(object), object)
-}
-
-export function extractQueryParameter(url, parameter) {
-	const parsedUrl = new URL(url)
-
-	return parsedUrl.searchParams.get(parameter)
 }
